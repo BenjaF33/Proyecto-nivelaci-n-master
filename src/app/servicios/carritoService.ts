6bd9e1e2584cc014b397835543132c067b8611c0
BenjaF33 <benjalucasfabris@gmail.com>
2026-05-29 10:45:07 -0300

    estoy haciedno lo del stock

--- src/app/servicios/carritoService.ts
+++ src/app/servicios/carritoService.ts
@@ -13,30 +13,55 @@ export class CarritoService {
 
   addProducto(producto: any) {
 
-    const productoExistente = this.productos.find(p => p.id === producto.id);
+    const productoExistente = this.productos.find(
+      p => p.id === producto.id
+    );
+
+    if (producto.stock === 0) {
+      producto.disponible = false;
+      return;
+    }
 
     if (productoExistente) {
       productoExistente.cantidad += 1;
     } else {
-      this.productos.push({ ...producto, cantidad: 1 });
+      this.productos.push({
+        ...producto,
+        cantidad: 1
+      });
     }
+
+    producto.stock--;
+
   }
 
   editarProducto(accion: string, producto: any) {
+
     if (accion === 'sumar') {
+
+      if (producto.stock <= 0) {
+        alert('No hay más stock');
+        return;
+      }
+
       producto.cantidad += 1;
+      producto.stock--;
+
     } else if (accion === 'restar') {
       if (producto.cantidad > 1) {
         producto.cantidad -= 1;
+        producto.stock++;
+        producto.disponible = true;
       }
     }
   }
 
   eliminarProducto(producto: any) {
-    this.productos = this.productos.filter(p => p.id !== producto.id);
-
-    console.log('Producto eliminado:', producto);
-    console.log('Productos restantes:', this.productos);
+    producto.stock += producto.cantidad;
+    producto.disponible = true;
+    this.productos = this.productos.filter(
+      p => p.id !== producto.id
+    );
   }
 
   vaciarCarrito() {
